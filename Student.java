03c8bbb493057c89b94093221fb9cd52d317e11c
kyungmin2322 <145512842+kyungmin2322@users.noreply.github.com>
2023-10-18 12:20:39 +0900

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,5 +1,27 @@
 package org.javaro.lecture;
 
 public class Student {
-
+	String Num, Name;
+	int Class;
+	Exam [] exam;
+	public Student(String Num, String Name,  int Class) {
+		this.Num = Num;
+		this.Name = Name;
+		exam = new Exam[Class];
+		for(int i = 0; i < exam.length; i++) {
+			exam[i] = new Exam(); }
+	}
+	public void setNum(String Num) { this.Num = Num; }
+	public void setName(String Name) { this.Name = Name; }
+	public String getName() { return this.Name; }
+	
+	public int getGPA() { 
+		int sum = 0;
+		for(int i = 0; i < exam.length; i++) sum = sum + this.exam[i].getScore();
+		return sum;
+	}
+	public void printGPA() {
+		for(int i = 0; i < exam.length; i++)
+			System.out.println(this.exam[i].getClassName() + this.exam[i].getScore());
+		System.out.println("합계 = " + this.getGPA()); }
 }
