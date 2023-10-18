bb93634eee752a82db1c57f696ce3a0ee280376a
kyungmin2322 <145512842+kyungmin2322@users.noreply.github.com>
2023-10-18 12:20:54 +0900

    Update Exam.java

--- Exam.java
+++ Exam.java
@@ -1,5 +1,18 @@
 package org.javaro.lecture;
 
 public class Exam {
-
+	String ClassName;
+	int Score;
+	
+	public void setClassName(String ClassName) {
+		this.ClassName = ClassName; }
+	public String getClassName() {
+		return this.ClassName; }
+	public void setScore(int Score) {
+		this.Score = Score; }
+	public int getScore() {
+		return this.Score; }
+	public void setExamScore(String ClassName, int Score) {
+		this.ClassName = ClassName;
+		this.Score = Score; }
 }
