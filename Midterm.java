938d7f8b785cb00a3faf8d36fe5e8d0c4eee5dc0
kyungmin2322 <145512842+kyungmin2322@users.noreply.github.com>
2023-10-18 12:21:26 +0900

    Update Midterm.java

--- Midterm.java
+++ Midterm.java
@@ -1,5 +1,9 @@
 package org.javaro.lecture;
 
 public class Midterm {
-
-}
+	Student stud1 = new Student("2019001", "홍길동", 3);
+	stud1.exam[0].setExamScore("자바", 88);
+	stud1.exam[1].setExamScore("C", 91);
+	stud1.exam[2].setExamScore("C++", 92);
+	stud1.printGPA();
+	}
